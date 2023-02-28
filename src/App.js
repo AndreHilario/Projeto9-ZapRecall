c3d47ed6979e50e8a97f1bfd095288eca9ce949d
Andre Hilario <andre18hilario@gmail.com>
2023-02-28 16:40:08 -0300

    React components and first screen(correction)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Header from "./constants/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
-import logo from "./assets/logo.png"
+import logo from "./assets/logo.png";
 
 export default function App() {
   return (
